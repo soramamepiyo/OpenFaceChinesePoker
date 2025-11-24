9ecedffd39e7ff2812c3953633f1b3e7f3589755
中西空 <sora10201020@gmail.com>
2025-11-24 11:30:03 +0900

    ハンドの強さを評価するクラスを仮定義

---
+++ Assets/Scripts/Class/HandEvaluator.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class HandEvaluator : MonoBehaviour
+{
+    // @todo: 5枚のカードから役を返す
+    // Jokerが存在することに注意
+    // memo: カードの定義は、Card.csを参照
+
+    // @todo: 2種類の5枚のカードから、どちらが勝っているかを返す
+    // チョップ(引き分け)も存在するため、返り値はboolで定義できないことに注意
+}
